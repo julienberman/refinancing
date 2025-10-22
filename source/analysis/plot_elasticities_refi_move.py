d18f907ba47e0672f570949b494f0dc55386516b
Julien Berman <julien.d.berman@gmail.com>
2025-10-22 14:55:49 -0400

    plot elasticities

--- source/analysis/plot_elasticities_refi_move.py
+++ source/analysis/plot_elasticities_refi_move.py
@@ -14,28 +14,24 @@ def main():
     sce_housing = pd.read_csv(INDIR_SCE / 'sce_housing.csv')
     sce_credit = pd.read_csv(INDIR_SCE / 'sce_credit.csv')
     mortgage_rates = pd.read_csv(INDIR_MORTGAGE_RATES / 'mortgage30us.csv')
-    
+    print(sce_housing['mortgage_rate_type'].value_counts())
     data = (
-        pd.concat([sce_housing, sce_credit])
+        sce_housing
+        # pd.concat([sce_housing, sce_credit])
         .merge(mortgage_rates, on='date', how='left')
         .query('residence_status_own == "yes" and mortgage_rate_on_own_home.notna()')
         .assign(likelihood_refi_1y_ahead = lambda x: x['likelihood_refi_1y_ahead'] / 100)
         .assign(likelihood_sell_home_1y_ahead = lambda x: x['likelihood_sell_home_1y_ahead'] / 100)
-        .assign(mortgage_rate_diff = lambda x: x['mortgage_rate_on_own_home'] - x['mortgage_rate'])
-        .select(columns=['sce_id', 'date', 'mortgage_rate', 'mortgage_rate_on_own_home', 'mortgage_rate_diff', 'likelihood_refi_1y_ahead', 'likelihood_sell_home_1y_ahead'])
+        .assign(likelihood_move_1y_ahead = lambda x: x['likelihood_move_1y_ahead'] / 100)
+        .assign(mortgage_rate_diff = lambda x: x['mortgage_rate_on_own_home'] - x['mortgage_rate_avg_current'])
+        .query('-12.5 < mortgage_rate_diff < 12.5')
+        .query('mortgage_rate_type == "variable"')
+        .select(columns=['sce_id', 'date', 'mortgage_rate', 'mortgage_rate_on_own_home', 'mortgage_rate_diff', 'likelihood_refi_1y_ahead', 'likelihood_sell_home_1y_ahead', 'likelihood_move_1y_ahead'])
     )
-    
-    # print("\nTop 50 highest mortgage rates and their SCE IDs:")
-    # print(
-    #     data
-    #     .sort_values('mortgage_rate_on_own_home', ascending=False)
-    #     .head(50)
-    #     [['sce_id', 'mortgage_rate_on_own_home']]
-    # )
     
     plot_scatter(
         data, 
-        x_var='mortgage_rate', 
+        x_var='mortgage_rate_diff', 
         y_var='likelihood_refi_1y_ahead', 
         out_file=OUTDIR / 'elasticity_refi',
         fit_line=True,
@@ -45,9 +41,19 @@ def main():
     
     plot_scatter(
         data, 
-        x_var='mortgage_rate', 
+        x_var='mortgage_rate_diff', 
         y_var='likelihood_sell_home_1y_ahead', 
         out_file=OUTDIR / 'elasticity_sell_home',
+        fit_line=True,
+        show_equation=True,
+        output_format='png'
+    )
+    
+    plot_scatter(
+        data, 
+        x_var='mortgage_rate_diff', 
+        y_var='likelihood_move_1y_ahead', 
+        out_file=OUTDIR / 'elasticity_move',
         fit_line=True,
         show_equation=True,
         output_format='png'
