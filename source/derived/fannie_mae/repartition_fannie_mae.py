050da0e1f15974d00abd149fb1a2d4d98f1e4569
Julien Berman <julien.d.berman@gmail.com>
2025-11-15 22:36:00 -0500

    add repartition file #1

---
+++ source/derived/fannie_mae/repartition_fannie_mae.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import json
+from pathlib import Path
+
+from source.lib.helpers.utils import get_quarters
+
+def main():
+    with open('source/lib/config.json', 'r') as f:
+        CONFIG = json.load(f)
+
+    INDIR = Path("datastore/output/derived/fannie_mae")
+    OUTDIR = Path("datastore/output/derived/fannie_mae")
+    START_DATE, END_DATE = CONFIG['SAMPLE_START'], CONFIG['SAMPLE_END']
+    QUARTERS = get_quarters(START_DATE, END_DATE)
+    CHUNKSIZE = CONFIG['CHUNKSIZE']
+    
+    for quarter in QUARTERS:
+        print(f"Processing {quarter}...")
+        df = pd.read_parquet(INDIR / "sflp_clean" / f"{quarter}.parquet")
+        quarter_outdir = OUTDIR / "sflp_clean" / quarter
+        quarter_outdir.mkdir(parents=True, exist_ok=True)
+        
+        for i in range(0, len(df), CHUNKSIZE):
+            df_part = df.iloc[i: i + CHUNKSIZE]
+            part = i // CHUNKSIZE
+            df_part.to_parquet(quarter_outdir / f"part_{part}.parquet", index = False)
+
+if __name__ == "__main__":
+    main()
+
